e0de4bb81e9dfc74d1cac198b1ec8a74b843e2de
Ricardo Rosa <rmsrosa@gmail.com>
2021-03-12 15:58:02 -0300

    add test

---
+++ tests/runtest.jl
@@ -0,0 +1,64 @@
+using ChainPlot
+using Flux
+using Plots
+
+gr()
+theme(:default)
+
+themes = [
+    :default
+    :dark
+    :ggplot2 # somehow throwing error
+    :juno
+    :lime
+    :orange
+    :sand
+    :solarized
+    :solarized_light
+    :wong
+    :wong2
+    :gruvbox_dark
+    :gruvbox_light
+]
+
+dl = Dense(2,3)
+display(plot(dl, title="$dl", titlefontsize=12))
+# savefig("dl.png")
+
+dr = RNN(3,5)
+display(plot(dr, title="$dr", titlefontsize=12))
+# savefig("dr.png")
+
+drc = Flux.RNNCell(3,5)
+display(plot(drc, title="$drc", titlefontsize=12))
+# savefig("drc.png")
+
+drlstm = Flux.LSTM(4,7)
+display(plot(drlstm, title="$drlstm", titlefontsize=12))
+# savefig("drlstm.png")
+
+nn = Chain(Dense(2,5), Dense(5,7,σ), Dense(7,2,relu),Dense(2,3))
+display(plot(nn, title="$nn", titlefontsize=10, xlabel="layer", xaxis=nothing))
+# savefig("nn.png")
+
+nnr = Chain(Dense(2,5,σ),RNN(5,4,relu), LSTM(4,4), Dense(4,3))
+display(plot(nnr, title="$nnr", titlefontsize=10, yaxis=true))
+# savefig("nnr.png")
+
+nnrl = Chain(Dense(5,8), RNN(8,20), LSTM(20,10), Dense(10,7))
+display(plot(nnrl, title="$nnrl", titlefontsize=12, yaxis=true))
+# savefig("nnrl.png")
+
+for t in themes
+    theme(t)
+    try        
+        display(plot(nnr, title="$nnr with theme $t", titlefontsize=8, yaxis=true))
+        # savefig("nnr_$t.png")
+    catch err
+        println("Error in chain plot with theme $t: $err")
+    end
+end
+
+theme(:default)
+
+nothing
