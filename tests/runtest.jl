98e956844d3690c81a2ae36eae2a48b87a311010
Ricardo Rosa <rmsrosa@gmail.com>
2021-03-12 15:59:29 -0300

    save example png plots

--- tests/runtest.jl
+++ tests/runtest.jl
@@ -23,37 +23,37 @@ themes = [
 
 dl = Dense(2,3)
 display(plot(dl, title="$dl", titlefontsize=12))
-# savefig("dl.png")
+savefig("tests/img/dl.png")
 
 dr = RNN(3,5)
 display(plot(dr, title="$dr", titlefontsize=12))
-# savefig("dr.png")
+savefig("tests/img/dr.png")
 
 drc = Flux.RNNCell(3,5)
 display(plot(drc, title="$drc", titlefontsize=12))
-# savefig("drc.png")
+savefig("tests/img/drc.png")
 
 drlstm = Flux.LSTM(4,7)
 display(plot(drlstm, title="$drlstm", titlefontsize=12))
-# savefig("drlstm.png")
+savefig("tests/img/drlstm.png")
 
 nn = Chain(Dense(2,5), Dense(5,7,σ), Dense(7,2,relu),Dense(2,3))
 display(plot(nn, title="$nn", titlefontsize=10, xlabel="layer", xaxis=nothing))
-# savefig("nn.png")
+savefig("tests/img/nn.png")
 
 nnr = Chain(Dense(2,5,σ),RNN(5,4,relu), LSTM(4,4), Dense(4,3))
 display(plot(nnr, title="$nnr", titlefontsize=10, yaxis=true))
-# savefig("nnr.png")
+savefig("tests/img/nnr.png")
 
 nnrl = Chain(Dense(5,8), RNN(8,20), LSTM(20,10), Dense(10,7))
 display(plot(nnrl, title="$nnrl", titlefontsize=12, yaxis=true))
-# savefig("nnrl.png")
+savefig("tests/img/nnrl.png")
 
 for t in themes
     theme(t)
     try        
         display(plot(nnr, title="$nnr with theme $t", titlefontsize=8, yaxis=true))
-        # savefig("nnr_$t.png")
+        savefig("tests/img/nnr_$t.png")
     catch err
         println("Error in chain plot with theme $t: $err")
     end
