a8ba76c17cb9570dd1e77fd09b8bb5122257b0e3
Ricardo Rosa <rmsrosa@gmail.com>
2021-03-13 09:56:02 -0300

    docstrings and chain dimensions

--- src/ChainPlot.jl
+++ src/ChainPlot.jl
@@ -15,6 +15,11 @@ lstm_verts = vcat([(1.0*sin(2π*n/20), 1.4 + 1.0*cos(2π*n/20)) for n=-10:10],
 lgru_verts = vcat([(0.6*sin(2π*n/20), 1.4 + 1.0*cos(2π*n/20)) for n=-10:10],
                   [(1.4*sin(2π*n/20), 1.4 + 1.4*cos(2π*n/20)) for n=-10:10])
 
+"""
+    layerplotattributes()
+
+Retrive plot attributes for each specific type of layer.
+"""                  
 layerplotattributes(::Any) = (ms = :circle, mc = :black)
 layerplotattributes(::Flux.Dense) = (ms = :circle, mc = :lightgreen)
 layerplotattributes(::Flux.RNNCell) = (ms = [Main.Plots.Shape(lrnn_verts), :circle], mc = [false, :lightblue])
@@ -22,6 +27,11 @@ layerplotattributes(::Flux.LSTMCell) = (ms = [Main.Plots.Shape(lstm_verts), :cir
 layerplotattributes(::Flux.GRUCell) = (ms = [Main.Plots.Shape(lgru_verts), :circle], mc = [false, :lightblue])
 layerplotattributes(r::Flux.Recur) = layerplotattributes(r.cell)
 
+"""
+    layeractivationfn()
+
+Retrive activation function of a given layer.
+"""
 layeractivationfn(::Any) = ""
 layeractivationfn(d::Flux.Dense) = string(Symbol(d.σ))
 layeractivationfn(r::Flux.RNNCell) = string(Symbol(r.σ))
@@ -29,12 +39,41 @@ layeractivationfn(r::Flux.LSTMCell) = "LSTM"
 layeractivationfn(r::Flux.GRUCell) = "GRU"
 layeractivationfn(r::Flux.Recur) = layeractivationfn(r.cell)
 
+"""
+    layerdimensions()
+
+Retrive dimensions of a given layer.
+"""
 layerdimensions(::Any) = (1,1)
 layerdimensions(l::Flux.Dense) = size(l.W)
 layerdimensions(l::Flux.RNNCell) = (size(l.Wh)[2], size(l.Wi)[2])
 layerdimensions(l::Flux.LSTMCell) = (size(l.Wh)[2], size(l.Wi)[2])
 layerdimensions(l::Flux.GRUCell) = (size(l.Wh)[2], size(l.Wi)[2])
 layerdimensions(r::Flux.Recur) = layerdimensions(r.cell)
+
+fixed_input_dim_layers = (Flux.Dense, Flux.Recur, Flux.RNNCell, Flux.LSTMCell, Flux.GRUCell) # list of types of layers with fixed input dimensions
+
+"""
+    get_chain_dimensions(m::Flux.Chain)
+
+Get input and output dimensions of each layer of a Flux.Chain.
+
+The first layer is required to be a layer with fixed input dimensions,
+such as Flux.Dense or a Flux.Recur.
+"""
+function get_chain_dimensions(m::Flux.Chain)
+
+    if !(m.layers[1] isa Union{fixed_input_dim_layers...})
+        throw(ArgumentError("The first layer is required to be a layer with fixed input dimensions, such as Flux.Dense or a Flux.Recur."))
+    end
+
+    chain_dimensions = fill(Int16(1),length(m.layers)+1)
+    chain_dimensions[1] = layerdimensions(m.layers[1])[2] # input dimensions
+    for (ln, l) in enumerate(m.layers)
+        chain_dimensions[ln+1] = layerdimensions(l)[1]
+    end
+    return chain_dimensions
+end
 
 """
     plot(m::Flux.Chain)
@@ -44,7 +83,8 @@ Plot a Flux.Chain neural network.
 ☡ Currently only works with Dense and Recurrent layers.
 """
 @recipe function plot(m::Flux.Chain)
-    max_width = maximum(layerdimensions(l)[1] for l in m.layers)
+    chain_dimensions = get_chain_dimensions(m)
+    max_width = maximum(chain_dimensions)
 
     axis --> false
     xrotation   --> 60
@@ -56,13 +96,16 @@ Plot a Flux.Chain neural network.
     seriescolor --> :gray
 
     # draw connections
+    ni = chain_dimensions[1]
     for (ln, l) in enumerate(m.layers)
         @series begin
             # arrow --> true
-            nj, ni = layerdimensions(l)
+            # nj, ni = layerdimensions(l)
+            nj = chain_dimensions[ln+1]
             layer_center = [ni,nj]./2
             dataseries = hcat([[i,j] for i in 1:ni for j in 1:nj]...) .- layer_center
             dataseries = map(x -> ((x + max_width/2)/(max_width+1)), dataseries)
+            ni=nj
             return [ln,ln + 1], dataseries
         end
     end
@@ -78,12 +121,12 @@ Plot a Flux.Chain neural network.
         return [1], dataseries        
     end
 
-    # draw hidden layer neurons
+    # draw hidden and output layer neurons
     for (ln, l) in enumerate(m.layers)
         @series begin
             markershape --> layerplotattributes(l).ms
             markercolor --> ifelse(ln == length(m), [:transparent,:orange], layerplotattributes(l).mc)
-            nj, ni = layerdimensions(l)
+            nj = chain_dimensions[ln+1]
             layer_center = nj/2
             dataseries = hcat([[j, j] for j in 1:nj]...) .- layer_center
             dataseries = map(x -> ((x + max_width/2)/(max_width+1)), dataseries)
@@ -94,7 +137,7 @@ Plot a Flux.Chain neural network.
     # display activation functions
     for (ln, l) in enumerate(m.layers)
         @series begin
-            nj, ni = layerdimensions(l)
+            nj = chain_dimensions[ln+1]
             series_annotations --> Main.Plots.series_annotations([layeractivationfn(l)], Main.Plots.font("Sans", 8))
             return [ln+1], [(nj/2 + 1 + max_width/2)/(max_width+1)]
         end
@@ -102,7 +145,7 @@ Plot a Flux.Chain neural network.
 
 end
 
-@recipe function plot(d::Union{Flux.Dense, Flux.Recur, Flux.RNNCell})
+@recipe function plot(d::Union{Flux.Dense, Flux.Recur, Flux.RNNCell, Flux.LSTMCell, Flux.GRUCell})
     Flux.Chain(d)
 end
 
@@ -126,4 +169,6 @@ Useful when the neural network starts with a variable-input layer, such as a con
 """
 @recipe function plot((m,s)::Tuple{Flux.Chain,Array})
     return (m, size(s))
+end
+
 end
